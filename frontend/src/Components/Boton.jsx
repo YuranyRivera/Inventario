16717b77d8e45d06fed32c37fc7480a4ea5cd5a3
Yurany Rivera <yuranidelacruz2006@gmail.com>
2024-11-21 11:06:59 -0500

    nuevo

---
+++ frontend/src/Components/Boton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BotonPrincipal = ({ Text, className = '', onClick, isSelected, additionalClasses = '' }) => {
+  return (
+    <button
+      className={`bg-[#00A305] relative cursor-pointer font-semibold overflow-hidden z-10 border border-verde group w-[180px] h-[50px] py-[10px] rounded-[15px] mt-3 self-center `}
+      onClick={onClick}
+    >
+      <span className="relative z-10 text-white group-hover:text-black text-[18px] duration-500">
+        {Text}
+      </span>
+     
+    </button>
+  );
+};
+
+
+
+export default BotonPrincipal;
--- frontend/src/Pages/Principal/Inicio.jsx
+++ frontend/src/Pages/Principal/Inicio.jsx
@@ -1,54 +1,66 @@
 import React from 'react';
-
+import Input from '../../Components/Input';
+import Boton from '../../Components/Boton'
 const Login = () => {
   return (
-    <div className="h-screen w-full overflow-hidden">
+    <div className="h-screen w-full overflow-hidden relative">
       <img 
         src="/Img/colegio.png" // Aquí se usa la ruta relativa desde public
         alt="Imagen del colegio" 
         className="w-full h-full object-cover" 
       />
-       <div className="absolute inset-0 bg-black opacity-10"></div> 
 
-      <div className="absolute w-[90%] h-[90%] top-[5%] left-2/4 transform -translate-x-1/2 bg-white bg-opacity-70 p-10 rounded-lg flex">
+       <div className="absolute inset-0 bg-black opacity-10"></div> 
+        {/* fondo negro */}
+      <div className="absolute w-[80%] h-[80%] top-[10%] left-2/4 transform -translate-x-1/2 bg-white bg-opacity-50  rounded-lg flex">
         {/* Columna izquierda (título y eslogan) */}
-        <div className="w-1/2 flex flex-col justify-center space-y-4">
-          <h2 className="text-4xl font-bold text-gray-800 text-center">Bienvenido al Sistema</h2>
-          <p className="text-lg text-gray-600 text-center">Accede con tus credenciales para continuar</p>
+        <div className="w-1/2 flex flex-col justify-center space-y-4 p-20">
+          <h2 className="font-serif text-[50px] break-words   text-white  font-montagu">¡Trabajemos juntos!
+</h2>
+          <p className="font-fans text-lg text-[30px] text-white font-bold">Para crear un ambiente positivo y colaborativo </p>
         </div>
 
         {/* Columna derecha (formulario de login) */}
-        <div className="w-1/2 flex justify-center items-center">
-          <form className="space-y-4 w-full max-w-sm">
+        <div className="w-1/2  bg-white flex flex-col justify-center  items-center">
+     <div className=" mb-10 "> 
+    <img 
+      src="/Img/logo.png" // Aquí se usa la ruta relativa desde public
+      alt="Imagen del colegio" 
+      className="w-32 h-auto" // Ajusta el tamaño del logo si es necesario
+    />
+  </div>
+          <form className="  space-y-4 w-full max-w-sm justify-center  items-center">
+            <h2 className=' text-center text-4xl font-josefin flex-auto '>Iniciar Sesion</h2>
             <div>
-              <label htmlFor="username" className="block text-sm font-semibold text-gray-700">Usuario</label>
-              <input 
+           
+              <Input 
                 type="text" 
+           
                 id="username" 
                 name="username" 
-                className="w-full p-3 mt-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                className="w-full p-10 mt-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
                 placeholder="Ingresa tu usuario"
               />
             </div>
 
             <div>
-              <label htmlFor="password" className="block text-sm font-semibold text-gray-700">Contraseña</label>
-              <input 
+          
+              <Input 
                 type="password" 
                 id="password" 
                 name="password" 
-                className="w-full p-3 mt-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                className="w-full  mt-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
                 placeholder="Ingresa tu contraseña"
               />
             </div>
 
             <div className="flex justify-center">
-              <button type="submit" className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition duration-300">Iniciar sesión</button>
+              <Boton type="submit" Text=" Iniciar sesión"></Boton>
             </div>
           </form>
         </div>
       </div>
-    </div>
+    </div> 
   );
 };
 
